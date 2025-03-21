17b4127e20a8920d0eaf5e630a515fdda285402e
= <camposdlucasoli@gmail.com>
2025-03-21 20:27:41 -0300

    feat : consultando os produtos inseridos e verificando se eles tem desconto

---
+++ src/main/java/ecommerce_api/ecommerce_api/repository/ProductWithDiscountRepository.java
@@ -0,0 +1,15 @@
+package ecommerce_api.ecommerce_api.repository;
+
+import ecommerce_api.ecommerce_api.model.ProductWithDiscountEntity;
+import ecommerce_api.ecommerce_api.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductWithDiscountRepository extends JpaRepository<ProductWithDiscountEntity, Long> {
+    @Query("select p from product_main p order by p.id desc limit 1")
+    Produto findLastInserted();
+
+    boolean existsByProduto(Produto produto);
+}
