db925e605ea7d5782b6b8c8a828baef051896137
= <camposdlucasoli@gmail.com>
2025-03-21 20:26:43 -0300

    update : desativando scheduling na API

--- src/main/java/ecommerce_api/ecommerce_api/EcommerceApiApplication.java
+++ src/main/java/ecommerce_api/ecommerce_api/EcommerceApiApplication.java
@@ -2,8 +2,10 @@ package ecommerce_api.ecommerce_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class EcommerceApiApplication {
 
     public static void main(String[] args) {
