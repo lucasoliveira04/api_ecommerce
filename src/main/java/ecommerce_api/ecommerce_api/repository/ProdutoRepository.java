67d5f399478ca2c9fbf4ebe8aa3b38ad77d0806b
= <camposdlucasoli@gmail.com>
2025-03-21 20:29:56 -0300

    update : adicionando anotação @Repository

--- src/main/java/ecommerce_api/ecommerce_api/repository/ProdutoRepository.java
+++ src/main/java/ecommerce_api/ecommerce_api/repository/ProdutoRepository.java
@@ -2,6 +2,8 @@ package ecommerce_api.ecommerce_api.repository;
 
 import ecommerce_api.ecommerce_api.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 }
