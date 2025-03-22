4ea1665c807931bf5fb260e74a024402f9891433
= <camposdlucasoli@gmail.com>
2025-03-22 15:40:34 -0300

    feat : Dto da entidade: ProductWithDiscountEntity.java

--- src/main/java/ecommerce_api/ecommerce_api/dto/ProductWithDiscountDto.java
+++ src/main/java/ecommerce_api/ecommerce_api/dto/ProductWithDiscountDto.java
@@ -1,7 +1,7 @@
 package ecommerce_api.ecommerce_api.dto;
 
-import ecommerce_api.ecommerce_api.model.Produto;
+import ecommerce_api.ecommerce_api.model.Product.Produto;
 
 import java.time.LocalDateTime;
 
-public record ProductWithDiscountDto(Produto produto, LocalDateTime initDiscount, LocalDateTime endDiscount) {}
+public record ProductWithDiscountDto(Produto produtoId, LocalDateTime initDiscount, LocalDateTime endDiscount) {}
