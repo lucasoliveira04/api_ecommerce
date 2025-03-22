530c135399cd2cb9c713ce49f92db3cd74305497
= <camposdlucasoli@gmail.com>
2025-03-22 15:38:52 -0300

    feat : nova rota que aplica o service de data limite no desconto

--- src/main/java/ecommerce_api/ecommerce_api/controller/ProductController.java
+++ src/main/java/ecommerce_api/ecommerce_api/controller/ProductController.java
@@ -1,8 +1,11 @@
 package ecommerce_api.ecommerce_api.controller;
 
 import ecommerce_api.ecommerce_api.dto.ProdutoDto;
-import ecommerce_api.ecommerce_api.services.CreatedProductService;
+import ecommerce_api.ecommerce_api.model.Product.ApplyDateLimitDiscountRequest;
+import ecommerce_api.ecommerce_api.services.discount.service.ApplyDateInitAndEndDiscountProduct;
+import ecommerce_api.ecommerce_api.services.discount.service.CreatedProductService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -10,13 +13,20 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "http://localhost:5173")
 public class ProductController {
     private final CreatedProductService createdProductService;
+    private final ApplyDateInitAndEndDiscountProduct applyDateInitAndEndDiscountProduct;
 
-    public ProductController(CreatedProductService createdProductService) {
+    public ProductController(CreatedProductService createdProductService, ApplyDateInitAndEndDiscountProduct applyDateInitAndEndDiscountProduct) {
         this.createdProductService = createdProductService;
+        this.applyDateInitAndEndDiscountProduct = applyDateInitAndEndDiscountProduct;
     }
 
     @PostMapping("/create")
     public ResponseEntity<?> create(@RequestBody ProdutoDto produtoDto) {
         return createdProductService.create(produtoDto);
+    }
+
+    @PostMapping("/apply/limit/date/discount")
+    public ResponseEntity<?> applyDiscount(@RequestBody ApplyDateLimitDiscountRequest request) {
+        return applyDateInitAndEndDiscountProduct.apply(request);
     }
 }
